ccefd21efa25fbb29eb5de3c1bd4b07f4953070c
Jukka Keinänen <jukka.keinane@gmail.com>
2026-02-08 09:51:32 +0200

    improve comment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-//Serve static files from /assets
+//Serve any static files from /assets folder
 app.use("/assets", express.static(path.join(process.cwd(), "assets")));
 
 app.listen(port, () => {
