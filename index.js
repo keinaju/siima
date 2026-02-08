cc459035add15d7545cc3dc7e82e20b74bfd88f2
Jukka Keinänen <jukka.keinane@gmail.com>
2026-02-08 09:57:40 +0200

    change routing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //Serve any static files from /assets folder
-app.use("/assets", express.static(path.join(process.cwd(), "assets")));
+app.use("/", express.static(path.join(process.cwd(), "assets")));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
