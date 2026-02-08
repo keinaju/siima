5a44110e06cc1f2214376af567c5c10b84e7e29e
Jukka Keinänen <jukka.keinane@gmail.com>
2026-02-08 09:32:11 +0200

    add routing of static files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello Siima!");
 });
+
+//Serve static files from /assets
+app.use("/assets", express.static(path.join(process.cwd(), "assets")));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
